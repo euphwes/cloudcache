b1993f29017fb7c9318fdf5adac95f4ac73738d0
Wes Evans <euphwes@gmail.com>
2016-02-25 20:51:40 -0700

    Added Notebook model and initial database migration.

---
+++ api/models/Notebook.py
@@ -0,0 +1,19 @@
+from django.db.models import Model, CharField, ForeignKey, CASCADE
+from django.contrib.auth.models import User
+
+
+class Notebook(Model):
+    """ A cloudCache notebook, which may contain additional notebooks and/or notes. """
+
+    class Meta:
+        ordering = ('id',)
+
+    name = CharField(max_length=1024, blank=False)
+    owner = ForeignKey(User, related_name='notebooks')
+    parent = ForeignKey('self', on_delete=CASCADE, related_name='notebooks', null=True)
+
+    def __repr__(self):
+        return '<Notebook: {}>'.format(self.name)
+
+    def __str__(self):
+        return self.name
