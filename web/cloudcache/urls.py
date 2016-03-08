48cc1de6eab8714448dc46b32735b049aa6de974
Wes Evans <euphwes@gmail.com>
2016-03-07 20:49:02 -0700

    Added Bootstrap and jQuery to static stuff. Added basic navbar to login page.

--- web/cloudcache/urls.py
+++ web/cloudcache/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login as django_login_view
 
 from .views import HomePageView
 
@@ -7,7 +8,7 @@ from .views import HomePageView
 
 urlpatterns = [
     # url(r'^logout/', LogoutView.as_view(), name='logout'),
-    url(r'^login/', 'django.contrib.auth.views.login', name='login'),
+    url(r'^login/', django_login_view, name='login'),
     # url(r'^register/', UserRegistrationView.as_view(), name='register'),
     url(r'^$', HomePageView.as_view(), name='home'),
 ]
