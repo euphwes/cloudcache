fde5bde64f9be3b7b8f6f2a4a53a6d48f4877fa3
Wes Evans <euphwes@gmail.com>
2016-03-08 20:44:17 -0700

    Moved LoginRequiredMixin into a subpackage, for cleaning __init__ of cloudcache.views.

---
+++ web/cloudcache/views/mixins/__init__.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.decorators import login_required
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+class LoginRequiredMixin(object):
+    """ Simple class mixin to make a view require login. """
+
+    @classmethod
+    def as_view(cls, **kwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**kwargs)
+        return login_required(view)
