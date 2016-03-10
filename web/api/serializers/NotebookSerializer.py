bbdd574efb921c2802866cfbd385e8840a8a5334
Wes Evans <euphwes@gmail.com>
2016-03-10 10:01:12 -0700

    Notebook list API endpoint now displays only the current users notebooks, and only allows creating a notebook for themselves.

--- web/api/serializers/NotebookSerializer.py
+++ web/api/serializers/NotebookSerializer.py
@@ -1,4 +1,5 @@
 from cloudcache.models import Notebook
+from authentication.models import Account
 from rest_framework.serializers import HyperlinkedModelSerializer
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -13,3 +14,23 @@ class NotebookSerializer(HyperlinkedModelSerializer):
         extra_kwargs = {
             'id': {'read_only': True},    # Shouldn't be able to edit ID
         }
+
+
+    def __init__(self, *args, **kwargs):
+        """ Custom __init__ to make the browseable API a bit more user-friendly. """
+
+        super().__init__(*args, **kwargs)
+
+        try:
+            # Assume there is a context and nested HTTP request in the kwargs
+            # Only show Notebooks owned by the currently logged-in user in the `parent` dropdown
+            # Only show the currently logged-in user in the `owner` dropdown
+            user = kwargs['context']['request'].user
+            self.fields['parent'].queryset = Notebook.objects.filter(owner=user)
+            self.fields['owner'].queryset = Account.objects.filter(id=user.id)
+
+        except KeyError:
+            # This would pop if there is no `context` in the kwargs, or `request` in the context. This would just mean
+            # that the serializer is being used outside of a web context, therefore we can use the `parent` and `owner`
+            # default querysets
+            pass
--- web/api/views/public/__init__.py
+++ web/api/views/public/__init__.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from authentication.models import Account
 from cloudcache.models import Notebook
@@ -16,28 +19,54 @@ class AccountList(ListCreateAPIView):
 
 
 class AccountDetail(RetrieveUpdateDestroyAPIView):
-    """ API endpoint which allows retrieving details for, updating, or deleting for a specific user. """
+    """ API endpoint which allows retrieving details for, updating, or deleting a specific user. """
 
     queryset = Account.objects.all().order_by('id')
     serializer_class = AccountSerializer
     permission_classes = [IsAccountSelfOrReadOnly]
 
+# ----------------------------------------------------------------------------------------------------------------------
 
 class NotebookList(ListCreateAPIView):
+    """ API endpoint for listing and creating Notebooks. Requires authentication. """
 
     serializer_class = NotebookSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        if self.request.user.is_anonymous():
-            return Notebook.objects.none()
-        return Notebook.objects.filter(owner=self.request.user).order_by('id')
+        """ For listing (via GET), only show the Notebooks owned by the currently logged-in user. """
+        return Notebook.objects.filter(owner=self.request.user)
+
+
+    def post(self, request, *args, **kwargs):
+        """ When creating a new Notebook, only allow the user to create new Notebook for themselves. """
+
+        # Get the Account detail url for the currently logged-in user.
+        expected_owner = AccountSerializer(request.user, context={'request': request}).data['url']
+
+        # Compare to the owner specified by the POST request. If they are not the same, complain about it. A user can
+        # only create an notebook for their own account.
+        if request.data['owner'] != expected_owner:
+            message = 'You may only create notebooks for yourself.'
+            return Response({'message': message}, status=HTTP_400_BAD_REQUEST)
+
+        # Use the NotebookSerializer to build up the Notebook, checking for validity, etc
+        # If valid, save the object and return a detail-style view along with status 201
+        # If invalid, return error messages along with status 400
+        notebook_serializer = NotebookSerializer(data=request.data, context={'request': request})
+        if notebook_serializer.is_valid():
+            notebook_serializer.save()
+            return Response(notebook_serializer.data, status=HTTP_201_CREATED)
+
+        return Response(notebook_serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
 class NotebookDetail(RetrieveUpdateDestroyAPIView):
+    """ API endpoint which allows retrieving details for, updating, or deleting a specific Notebook. """
 
     serializer_class = NotebookSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        if self.request.user.is_anonymous():
-            return Notebook.objects.none()
+        """ For displaying (via GET), only show the Notebooks owned by the currently logged-in user. """
         return Notebook.objects.filter(owner=self.request.user).order_by('id')
