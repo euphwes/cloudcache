8885231f932b77d7c6236c468ffd4f65e7f5056b
Wes Evans <euphwes@gmail.com>
2016-03-17 16:46:59 -0600

    Added logic to parse flat notebook list into tree structure, and rename object/notebook keys to what Bootstrap-Treeview expects.

--- web/cloudcache/static/cloudcache/js/cc_home.js
+++ web/cloudcache/static/cloudcache/js/cc_home.js
@@ -38,33 +38,81 @@ function getTree() {
     ];
 }
 
+function treeify(list, idAttr, parentAttr, childrenAttr) {
+    if (!idAttr) idAttr = 'url';
+    if (!parentAttr) parentAttr = 'parent';
+    if (!childrenAttr) childrenAttr = 'nodes';
+
+    var treeList = [];
+    var lookup = {};
+    list.forEach(function(obj) {
+        lookup[obj[idAttr]] = obj;
+        obj[childrenAttr] = [];
+    });
+    list.forEach(function(obj) {
+        if (obj[parentAttr] != null) {
+            lookup[obj[parentAttr]][childrenAttr].push(obj);
+        } else {
+            treeList.push(obj);
+        }
+    });
+    return treeList;
+}
+
+
+function recursivelyRemoveEmptyNodesArrays(notebooks) {
+
+    for (var i = 0; i < notebooks.length; i++){
+        nb = notebooks[i];
+        if (nb.nodes.length == 0) {
+            delete nb.nodes;
+        }
+        else {
+            nb.nodes = recursivelyRemoveEmptyNodesArrays(nb.nodes);
+        }
+    }
+
+    return notebooks;
+}
+
 
 function getUserNotebooks() {
     var get_url = '/api/notebooks/';
 
-    var onSuccess = function(jqXHR) {
-        console.log(jqXHR);
-    };
+    var notebooks;
 
     // Make the API GET call
-    return $.ajax({
+    $.ajax({
         url: get_url,
         type: 'GET',
         timeout: 1000,
-        success: onSuccess,
+        success: function(notebooks) {
+
+            for (var i = 0; i < notebooks.length; i++) {
+                notebooks[i].text = notebooks[i].name;
+                notebooks[i].nodes = notebooks[i].notebooks;
+                delete notebooks[i].name;
+                delete notebooks[i].notebooks;
+            }
+
+            notebooks = treeify(notebooks);
+            notebooks = recursivelyRemoveEmptyNodesArrays(notebooks);
+
+            $('#tree').treeview({
+                data: notebooks,
+                levels: 3,
+                backColor: '#f5f5f5',
+                selectedBackColor: '#446E9B',
+                showBorder: false,
+            });
+
+        }
     });
+
 
 }
 
 $(function(){
-
-    $('#tree').treeview({
-        data: getTree(),
-        levels: 3,
-        backColor: '#f5f5f5',
-        selectedBackColor: '#446E9B',
-        showBorder: false,
-    });
 
     // Set all ajax calls to send the CSRF token
     // Do *not* send the CSRF token if the request is cross-domain
