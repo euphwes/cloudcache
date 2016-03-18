3db30c3703d10e6e812e7887f4e388d07779ce3f
Wes Evans <euphwes@gmail.com>
2016-03-18 13:47:06 -0600

    Clean up and modularize the home page JS code a bit. Add some comments.

--- web/cloudcache/static/cloudcache/js/cc_home.js
+++ web/cloudcache/static/cloudcache/js/cc_home.js
@@ -1,8 +1,13 @@
 
-function treeify(list, idAttr, parentAttr, childrenAttr) {
-    if (!idAttr) idAttr = 'url';
+/**
+ * General utility function for turning a flat list of objects into a nested structure. The ID, parent, and children
+ * attributes' names are configurable, but it's assumed that each object coming in has a unique ID, and that each object
+ * either identifies a parent object by ID or identifies a null parent (meaning it's the root of a tree).
+ **/
+function buildTrees(list, idAttr, parentAttr, childrenAttr) {
+    if (!idAttr) idAttr = 'id';
     if (!parentAttr) parentAttr = 'parent';
-    if (!childrenAttr) childrenAttr = 'nodes';
+    if (!childrenAttr) childrenAttr = 'children';
 
     var treeList = [];
     var lookup = {};
@@ -20,63 +25,76 @@ function treeify(list, idAttr, parentAttr, childrenAttr) {
     return treeList;
 }
 
-
-function recursivelyRemoveEmptyNodesArrays(notebooks) {
-
-    for (var i = 0; i < notebooks.length; i++){
-        nb = notebooks[i];
-        if (nb.nodes.length == 0) {
-            delete nb.nodes;
+/**
+ * Bootstrap-Treeview believes that all objects with a 'nodes' attribute have children, even if that attribute is an
+ * empty array, and will display a collapse/expand icon. We don't want that behavior, so if a notebook has no child
+ * notebooks, we'll delete the (empty) 'nodes' attribute. If there are child notebooks, recursively check them for empty
+ * 'nodes' attributes as well.
+ **/
+function deleteEmptyNodes(notebooks) {
+    notebooks.forEach(function(notebook) {
+        if (notebook.nodes.length == 0) {
+            delete notebook.nodes;
+        } else {
+            notebook.nodes = deleteEmptyNodes(notebook.nodes);
         }
-        else {
-            nb.nodes = recursivelyRemoveEmptyNodesArrays(nb.nodes);
-        }
-    }
-
+    });
     return notebooks;
 }
 
-
-function getUserNotebooks() {
-    var get_url = '/api/notebooks/';
-
-    var notebooks;
-
-    // Make the API GET call
-    $.ajax({
-        url: get_url,
-        type: 'GET',
-        timeout: 1000,
-        success: function(notebooks) {
-
-            for (var i = 0; i < notebooks.length; i++) {
-                notebooks[i].text = notebooks[i].name;
-                notebooks[i].nodes = notebooks[i].notebooks;
-                delete notebooks[i].name;
-                delete notebooks[i].notebooks;
-            }
-
-            notebooks = treeify(notebooks);
-            notebooks = recursivelyRemoveEmptyNodesArrays(notebooks);
-
-            $('#tree').treeview({
-                data: notebooks,
-                levels: 3,
-                backColor: '#f5f5f5',
-                selectedBackColor: '#446E9B',
-                showBorder: false,
-            });
-
-        }
+/**
+ * The Bootstrap-Treeview library requires each object in the hierarchy to have a 'text' attribute for displaying the
+ * node in the tree. We can just use the notebook's name, so we rename it to 'text'. Also, child objects in the tree
+ * fall under a 'nodes' attribute. This information is in the notebook's 'notebooks' attribute, which we can just rename
+ * to 'nodes'.
+ **/
+function renameFieldsForTree(notebooks) {
+    notebooks.forEach(function(notebook){
+        notebook.text = notebook.name;
+        delete notebook.name;
+        notebook.nodes = notebook.notebooks;
+        delete notebook.notebooks;
     });
-
-
+    return notebooks;
 }
 
-$(function(){
+/**
+ * Build a hierarchical structure of notebooks, so that the Bootstrap-Treeview library can display a tree in the
+ * sidebar. Pre- and post- processes the notebook objects to make sure they're suitable (proper field names, etc)
+ **/
+function buildTreeviewForNotebooks(notebooks) {
+    notebooks = renameFieldsForTree(notebooks);                   // name->text, notebooks->nodes
+    notebooks = buildTrees(notebooks, 'url', 'parent', 'nodes');  // turn flat list into nested tree structure
+    notebooks = deleteEmptyNodes(notebooks);                      // make sure we don't have any empty child arrays
 
-    // Set all ajax calls to send the CSRF token
-    // Do *not* send the CSRF token if the request is cross-domain
+    // Build the treeview in the #tree div in the sidebar, with specific options
+    $('#tree').treeview({
+        data: notebooks,
+        levels: 3,
+        backColor: '#f5f5f5',
+        selectedBackColor: '#446E9B',
+        showBorder: false,
+    });
+}
+
+/**
+ * Kick off the process to get the user's notebooks and parse into a tree for navigation in the sidebar, by making an
+ * API call to the endpoint for listing all notebooks
+ **/
+function getUserNotebooks() {
+    $.ajax({
+        url: '/api/notebooks/',
+        type: 'GET',
+        timeout: 1000,
+        success: buildTreeviewForNotebooks,
+    });
+}
+
+/**
+ * On document-ready
+ **/
+$(function(){
+    // Set all ajax calls to send the CSRF token. Do *not* send the CSRF token if the request is cross-domain
     $.ajaxSetup({
         beforeSend: function(xhr, settings) {
             if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
