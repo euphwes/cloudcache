4b6c8b4a556ed819b67ffc13ff15acb1df0f03cc
Wes Evans <euphwes@gmail.com>
2016-03-22 15:29:29 -0600

    Cleaned up JS code related to building breadcrumbs.

--- web/cloudcache/static/cloudcache/js/cc_home.js
+++ web/cloudcache/static/cloudcache/js/cc_home.js
@@ -81,56 +81,77 @@ function buildTreeviewForNotebooks(notebooks) {
     wireTreeEvents();
 }
 
+// We want to get the shortest column in the content pane, to append the current note to the end of that.
+// Check the height of each of the note-col divs inside the #notes-wrapper, find the shortest, and return that
+function getShortestColumn() {
+    var minHeight = 9999999;
+    var shortColumn;
+    $('#notes-wrapper').children().each(function() {
+        if ($(this).height() < minHeight) {
+            minHeight = $(this).height();
+            shortColumn = $(this);
+        }
+    });
+    return shortColumn;
+}
+
+// Build up a note div which contains inner note-title and note-contents class divs, with the title and content
+// of a note object retrieved from the API. Do a replace-all on note.content to turn newlines into HTML line breaks
+function buildNote(note) {
+    var title = $('<div>', {class: 'note-title'}).append(note.title);
+    var note_content = note.content.replaceAll('\r\n', '<br>').trim();
+    var content = $('<div>', {class: 'note-contents'}).append('<p>' + note_content + '</p>');
+    var note = $('<div>', {class: 'note'}).append(title, content);
+    getShortestColumn().append(note);
+}
+
+/**
+ * Build up the breadcrumbs for the current notebook structure. Start at the current notebook, then keep working up the
+ * tree until you reach the root. At each notebook, prepend an ol->li element with the notebook's name. All notebooks
+ * fall under a root element here which we'll call 'Notebooks'.
+ **/
+function buildBreadcrumbs(notebook) {
+
+    $('#breadcrumbs').empty();
+
+    // Make the last element the active element with the current notebook's name
+    var crumb = $('<li>').addClass('active').append(notebook.text);
+    $('#breadcrumbs').prepend(crumb);
+
+    // Keep climbing the tree, finding each parent notebook, until we reach the top. For each parent notebook, prepend
+    // an ol->li element with that notebook's name
+    var parent = $('#tree').treeview('getParent', notebook);
+    while (parent.selector != '#tree') {
+        var anchor = $('<a href="#">').append(parent.text);
+        var crumb = $('<li>').append(anchor);
+        $('#breadcrumbs').prepend(crumb);
+
+        parent = $('#tree').treeview('getParent', parent);
+    }
+
+    // Prepend the root element in the breadcrumbs, which we'll call "Notebooks"
+    var anchor = $('<a href="#">').append('Notebooks');
+    var crumb = $('<li>').append(anchor);
+    $('#breadcrumbs').prepend(crumb);
+}
+
 /**
  * Event handler for selecting a node in the treeview. Performs an API retrieval for each note in that notebook, builds
  * an element for it in the DOM, and appends it to the content pane.
  **/
 function handleNotebookSelected(event, notebook) {
 
-    // Build up the breadcrumbs for the current notebook structure
-    $('#breadcrumbs').empty();
-    $('#breadcrumbs').prepend('<li class="active">' + notebook.text + '</li>');
-
-    var parent = $('#tree').treeview('getParent', notebook);
-    while (parent.selector != '#tree') {
-        console.log(parent);
-        $('#breadcrumbs').prepend('<li><a href="#">' + parent.text.trim() + '</a></li>')
-        parent = $('#tree').treeview('getParent', parent);
-    }
-    $('#breadcrumbs').prepend('<li><a href="#">Home</a></li>')
-
-    // We want to get the shortest column in the content pane, to append the current note to the end of that.
-    // Check the height of each of the note-col divs inside the #notes-wrapper, find the sortest, and return that
-    var getShortestColumn = function() {
-        var minHeight = 9999999;
-        var shortColumn;
-        $('#notes-wrapper').children().each(function() {
-            if ($(this).height() < minHeight) {
-                minHeight = $(this).height();
-                shortColumn = $(this);
-            }
-        });
-        return shortColumn;
-    };
-
-    // Build up a note div which contains inner note-title and note-contents class divs, with the title and content
-    // of a note object retrieved from the API. Do a replace-all on note.content to turn newlines into HTML line breaks
-    var buildNote = function(note) {
-        var title = $('<div>', {class: 'note-title'}).append(note.title);
-        var note_content = note.content.replaceAll('\r\n', '<br>').trim();
-        var content = $('<div>', {class: 'note-contents'}).append('<p>' + note_content + '</p>');
-        var note = $('<div>', {class: 'note'}).append(title, content);
-        getShortestColumn().append(note);
-    };
+    buildBreadcrumbs(notebook);
 
     // Do an API get on each note contained by this notebook, and when it succeeds, run it through buildNote
     notebook.notes.forEach(function(note) {
-        $.ajax({
+        var blah = $.ajax({
             url: note,
             type: 'GET',
             timeout: 1000,
             success: buildNote,
         });
+        console.log(blah);
     });
 }
 
