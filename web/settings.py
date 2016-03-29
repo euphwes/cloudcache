853036b787ca0769b075e90d0055fdd6a50d2180
Wes Evans <euphwes@gmail.com>
2016-03-29 11:57:28 -0600

    Added dj-database-url to pip requirements.txt, and updated settings.py to use that to point to database.

--- web/settings.py
+++ web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,15 +78,9 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cloudcache',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': '',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(default=os.environ['DATABASE_URL']),
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
