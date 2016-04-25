031dbbe0f7b04fe5400dc1fba654bebc0aa8ea95
Wes Evans <euphwes@gmail.com>
2016-04-25 09:27:03 -0600

    Deleted cc_home.js and core.js, leftover files from before. Moved last remaining core.js utility functions to app.js util object.

--- cloudcache/assets/js/cloudcache/app.js
+++ cloudcache/assets/js/cloudcache/app.js
@@ -28,6 +28,26 @@ $(function(){
         },
 
         /**
+         * Returns a function, that, as long as it continues to be invoked, will not be triggered. The function will be called
+         * after it stops being called for N milliseconds. If `immediate` is passed, trigger the function on the leading edge,
+         * instead of the trailing.
+         **/
+        debounce: function(func, wait, immediate) {
+            var timeout;
+            return function() {
+                var context = this, args = arguments;
+                var later = function() {
+                    timeout = null;
+                    if (!immediate) func.apply(context, args);
+                };
+                var callNow = immediate && !timeout;
+                clearTimeout(timeout);
+                timeout = setTimeout(later, wait);
+                if (callNow) func.apply(context, args);
+            };
+        },
+
+        /**
          * General utility function for turning a flat list of objects into a nested structure. The ID, parent, and
          * children attribute names are configurable, but it's assumed that each object coming in has a unique ID, and
          * that each object either identifies a parent object by ID or identifies an explicit null parent, meaning it's
