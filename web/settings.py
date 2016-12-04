ec7b6f454e4d697ea40eeff96a260b445780c5e7
Wes Evans <mark.evans@thebreakawaygroup.com>
2016-12-03 22:10:17 -0700

    Added settings values so DRF API endpoints will accept both basic and session authentication.

--- web/settings.py
+++ web/settings.py
@@ -117,6 +117,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
